9a6a87492781ad86a05dc5918ed00d64aa4cb727
Martsim <Martsim@users.noreply.github.com>
2015-03-16 15:25:56 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
-class Main{
-    public static void main(String[] args){
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JOptionPane;
 
-        Permutatsioon tulemus = new Permutatsioon(4);
-        System.out.println(tulemus.vastus());
-        
-        String message = "Tere tulemast kasutama n-mõõtmelise ruutmaatriksi determinandi arvutamise programmi.";
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// tervitus
+		String message = "Tere tulemast kasutama n-mõõtmelise ruutmaatriksi determinandi arvutamise programmi.";
 		JOptionPane.showMessageDialog(null, message,"Tere!",1);
-		message = "Valige '1' Demo programmiks,\n '2' Andmete sisestamiseks,\n '3' Eelnevalt sisestatud andmetega programm.";
+		// kuidas soovite programmi kasutada? 1) Random 2) sisestate andmed
+		message = "Valige '1' Demo programmiks,\n '2' Andmete sisestamiseks.";
 		int valik = Integer.parseInt(JOptionPane.showInputDialog(null, message, "Valik", 1));
 		
 		if (valik == 1) {
-			
 			Demo();
 		} else if (valik == 2) {
 			ArrayList<ArrayList<Double>> andmed = new ArrayList<ArrayList<Double>>();
@@ -26,9 +29,16 @@ class Main{
 			
 			JOptionPane.showMessageDialog(null, esitus + "maatriksi determinant on " + tulemus ,"Tere!",1);
 		}
-    }
-    public static ArrayList<ArrayList<Double>> sisestamine(){
-		ArrayList<Double> Rida = new ArrayList<Double>();		
+	}
+	
+	public static ArrayList<ArrayList<Double>> sisestamine(){
+		ArrayList<ArrayList<Double>> loodav = new ArrayList<ArrayList<Double>>();
+		String arv = JOptionPane.showInputDialog(null, "Sisesta maatriksi suurus", "Andmete sisestamine",JOptionPane.QUESTION_MESSAGE);
+		int suurus = Integer.parseInt(arv);
+		
+		for (int i = 0; i<suurus; i++){
+			
+			ArrayList<Double> Rida = new ArrayList<Double>();		
 			String teade = "Palun sisesta "+(i+1)+". rea arvud tühikuga eraldatult.";
 			String sisse = JOptionPane.showInputDialog(null, teade, "Andmete sisestamine",JOptionPane.QUESTION_MESSAGE);
 			
@@ -46,11 +56,11 @@ class Main{
 			loodav.add(Rida);
 		}
 		
-		return loodav;			
+		return loodav;		
 	}
 	
 	public static void Demo() {
-	ArrayList<ArrayList<Double>> demoAndmed = new ArrayList<ArrayList<Double>>();
+		ArrayList<ArrayList<Double>> demoAndmed = new ArrayList<ArrayList<Double>>();
 		
 		//genereeri demo maatriks
 		int suurus = 4;
@@ -76,4 +86,5 @@ class Main{
 		};
 		JOptionPane.showMessageDialog(null, esitus + "maatriksi determinant on " + tulemus ,"Tere!",1);
 	}
+
 }
