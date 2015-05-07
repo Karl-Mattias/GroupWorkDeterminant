ff93ec9ed344d2adb1878913f52da9ccbb2b9ed4
Karl-Mattias <Karl-Mattias@users.noreply.github.com>
2015-05-07 14:34:12 +0300

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -14,22 +14,21 @@ import javafx.scene.layout.GridPane;
 
 
 public class GUI extends Application {
-    static int RIDA = 2;
-    static int VEERG = 2;
-    static GridPane lahtrid;
+    private int suurus;
+    private GridPane lahtrid;
 
     public void start(Stage primaryStage) {
         try {
             BorderPane root = new BorderPane();
 
-            ChoiceBox menüü = new ChoiceBox(FXCollections.observableArrayList(
+            ChoiceBox menüü = new ChoiceBox<>(FXCollections.observableArrayList(
                     "1", "2", "3", "4","5"));
             Label text = new Label("Vali soovitud maatriksi suurus: ");
             text.setFont(Font.font(18));
             SplitPane splitpane = new SplitPane();
             root.setTop(splitpane);
             splitpane.getItems().addAll(text, menüü);
-            root.setMargin(splitpane, new Insets(30, 0, 0, 0));
+            BorderPane.setMargin(splitpane, new Insets(30, 0, 0, 0));
 
 
 
@@ -41,11 +40,10 @@ public class GUI extends Application {
                         lahtrid.setVgap(10);
                         lahtrid.setHgap(10);
 
-                        RIDA = (int) new_value + 1;
-                        VEERG = (int) new_value + 1;
-                        for (int i = 0; i < RIDA; i++) {
-                            for (int j = 0; j < VEERG; j++) {
-                                lahtrid.add(new TextField("0"), i, j);
+                        suurus = (int) new_value + 1;
+                        for (int i = 0; i < suurus; i++) {
+                            for (int j = 0; j < suurus; j++) {
+                                lahtrid.add(new TextField("0"), j, i);
                             }
                         }
                         root.setCenter(lahtrid);
@@ -71,19 +69,4 @@ public class GUI extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
-    static void nupuMeetod(GridPane lahtrid) {
-		
-		ArrayList A = new ArrayList();
-		for (Node TF : lahtrid.getChildren()){
-			int x = lahtrid.getColumnIndex(TF);
-			int Y = lahtrid.getRowIndex(TF);
-			String s = ((TextField)TF).getText();
-			System.out.println(s);
-	
-		}
-		//Maatriks MatA = new Maatriks(ArrayList<ArrayList<Double>> MatA);
-		//MatA.arvutaDeterminant();
-		
-	}
 }
